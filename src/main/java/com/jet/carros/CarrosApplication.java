aa2d556279a248cd1753a7190284482995103577
jeteraguilar <jeteraguilar@gmal.com>
2022-12-03 12:00:26 -0300

    testes nos métodos springboot banco h2 para teste

--- src/main/java/com/jet/carros/CarrosApplication.java
+++ src/main/java/com/jet/carros/CarrosApplication.java
@@ -2,7 +2,9 @@ package com.jet.carros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class CarrosApplication {
 
 	public static void main(String[] args) {
